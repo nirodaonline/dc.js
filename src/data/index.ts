c4487e80c34795e1293ed731fc816810cff304e9
Deepak Kumar <deepak@kreatio.com>
2024-03-31 19:00:51 +0530

    Export older classnames as well.

--- src/data/index.ts
+++ src/data/index.ts
@@ -6,3 +6,8 @@ export * from './filter-storage-helper.js';
 export * from './i-data-provider-behavior.js';
 export * from './cf-data-provider-behavior.js';
 export * from './i-simple-data-adapter-conf.js';
+
+// TODO: remove after examples and documentation are updated
+export {SimpleDataAdapter as CFSimpleAdapter } from './simple-data-adapter.js';
+export {MultiDataAdapter as CFMultiAdapter } from './multi-data-adapter.js';
+export {SimpleDataCapHelper as CFDataCapHelper } from './simple-data-cap-helper.js';
